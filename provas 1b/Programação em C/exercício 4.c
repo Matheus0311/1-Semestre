5753731369e38c2a00a291b3c8e255091551164f
Matheus Ribeiro <60912605+Matheus0311@users.noreply.github.com>
2020-05-06 23:23:50 -0400

    Add files via upload
    Provas 1b

---
+++ provas 1b/Programação em C/exercício 4.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //Biblioteca de tratamento de entrada/saída.
+#include <stdlib.h> //Biblioteca de implementação de Funções para diversas operações.
+#include <locale.h> //Biblioteca que especifica constantes de acordo com a localização específica, como moeda, data, etc.
+int main()//Início da função int main ().
+{
+    setlocale(LC_ALL, "Portuguese"); //LC_ALL Faz referência à todos os aspectos de localização.
+
