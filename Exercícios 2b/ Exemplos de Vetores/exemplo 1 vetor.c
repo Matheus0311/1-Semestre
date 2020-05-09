01c8a57032d29cf08dcc5b317121ac4f5b1bc769
Matheus Ribeiro <60912605+Matheus0311@users.noreply.github.com>
2020-05-08 21:05:16 -0400

    Update and rename Exercícios 2b/Exemplos Vetor/exemplo 1 vetor.c to  Exercícios 2b/ Exemplos de Vetores/exemplo 1 vetor.c

--- Exercícios 2b/ Exemplos de Vetores/exemplo 1 vetor.c
+++ Exercícios 2b/ Exemplos de Vetores/exemplo 1 vetor.c
@@ -3,13 +3,12 @@
 #include <locale.h>
 int main ()
 {
-    int A[8], B[8], i, j=1;
+    int A[8], B[8], i;
     setlocale(LC_ALL, "Portuguese");
     for (i=0; i<8; i++)
     {
-        printf("\nInforme o %dº número inteiro: ", j);
+        printf("\nInforme o %dÂº nÃºmero inteiro: ", i+1);
             scanf("%d", &A[i]);
-        j++;
     }
     for (i=0; i<8; i++)
     {
